4ad33e19a3ca6d42811fbbf7365beb439a0970ce
Michael DaSilva <programmedmikey@gmail.com>
2025-08-25 21:48:26 -0400

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="py-4 px-6 bg-gray-800 shadow-md flex justify-between items-center">
+        <h1 class="text-2xl font-bold text-white">Midnight Haven</h1>
+        <nav>
+            <ul class="flex space-x-4">
+                <li><RouterLink to="/" class="text-white hover:underline">Home</RouterLink></li>
+                <li><RouterLink to="/login" class="text-white hover:underline">Login</RouterLink></li>
+            </ul>   
+        </nav>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from '../components/Header.vue';
+</script>
 
 
 <template>
-    <h1>Home View</h1>
+    <div class="flex flex-col h-screen bg-gray-900 text-white">
+        <Header />
+        <div class="flex-grow flex items-center justify-center">
+            <h1 class="text-4xl font-bold">Welcome to Midnight Haven</h1>
+        </div>
+    </div>
 </template>
